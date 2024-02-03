fd6f2377dc19b4a9871b448345dcff6beed196bc
Poster <maksvologin@gmail.com>
2024-02-02 23:11:29 -0800

    Added plugin disable logic

--- src/main/java/org/poster/effortlesstab/EffortlessTab.java
+++ src/main/java/org/poster/effortlesstab/EffortlessTab.java
@@ -1,20 +1,31 @@
 package org.poster.effortlesstab;
 
+import org.bukkit.configuration.Configuration;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.poster.effortlesstab.events.TickOnServerEvent;
+import org.poster.effortlesstab.playerlist.PlayerList;
 
 public final class EffortlessTab extends JavaPlugin {
 
     TickOnServerEvent tickOnServerEvent = new TickOnServerEvent();
 
+    PlayerList playerList = new PlayerList();
+
     @Override
     public void onEnable() {
         /* Plugin startup logic
          */
-
         tickOnServerEvent.onServerTick(this);
 
         saveDefaultConfig();
+
+    }
+
+    @Override
+    public void onDisable() {
+        /* Plugin disable logic
+         */
+        playerList.clear();
     }
 
 }
